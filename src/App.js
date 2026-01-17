59c8ca64dd668ed0b8e27ef4ba7254ea71167e95
Kaan <bukealikaan@gmail.com>
2026-01-17 19:52:18 +0300

    website geliştirildi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import "./App.css"
 
 function Header(){
-  const [name, setName] = useState("");
   return(
-    <div style={{textAlign:"center"}}>
-      <h1>Welcome to Vectorial Labs!</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter Your Name'></input>
-      <h1>Welcome, {name}</h1>
+    <div>
+      <h1 className='aboutme'>Hakkımda</h1>
+      <div className='about'>
+        <div className='about_text'>
+          <p className='aboutme_paragraph'>Ben Ali Kaan Büke. Backend geliştirme alanına odaklanan bir lise öğrencisiyim. Sunucu tarafı yazılımlar, API yapıları ve uygulamaların arka plandaki işleyişi üzerine çalışmalar yapıyorum. Backend mimarisini, veri akışını ve sistemlerin nasıl ölçeklendiğini anlamaya ve uygulamaya odaklanıyorum.</p>
+          <p className='aboutme_paragraph'>Hedefim backend tarafında derinleşerek, sağlam ve sürdürülebilir sistemler geliştirebilecek seviyeye ulaşmak.</p>
+        </div>
+        {/*<img src='\images\5.png' className='image1'></img>*/}
+      </div>
+    </div>
+  )
+}
+
+function Theme(){
+  const [mode, setMode] = useState("dark");
+
+  useEffect(() => {
+    document.body.className = mode;
+  }, [mode])
+
+  return(
+    <div>
+      <button onClick={() => mode === "dark" ? setMode("light") : setMode("dark")}>{mode}</button>
     </div>
   )
 }
