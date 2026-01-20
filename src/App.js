e165dfc365fdf7e3ae0052bcf687ab9ce170089f
Kaan <bukealikaan@gmail.com>
2026-01-20 18:04:05 +0300

    started to code footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Header from './components/Header.js';
 import About from './components/About.js';
 import Projects from './components/Projects.js';
+import Footer from './components/Footer.js';
 
 function App(){
   return(
@@ -9,6 +10,7 @@ function App(){
       <Header />
       <About />
       <Projects />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+function Footer(){
+    return (
+        <footer className="footer">
+            <h3>© 2026 Ali Kaan Büke. All rights reserved.</h3>
+            <div className="footer-container">
+                <div className="footer-contact">
+                    <h2>Contact</h2>
+                    <p>Email: bukealikaan@gmail.com</p>
+                </div>
+                <div className="social-media">
+                    <h2>Follow Me!</h2>
+                    <a href="https://www.instagram.com/bukealikaan/" target="_blank">Instagram</a>
+                    <a href="https://x.com/bukealikaan" target="_blank">X</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
