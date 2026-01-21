f087313a439677306f3b923673f24c3c49165a71
Kaan <bukealikaan@gmail.com>
2026-01-21 12:48:14 +0300

    added icons to the footer

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export function About(){
             <p className='aboutme_paragraph'>Ben Ali Kaan Büke. Backend geliştirme alanına odaklanan bir lise öğrencisiyim. Sunucu tarafı yazılımlar, API yapıları ve uygulamaların arka plandaki işleyişi üzerine çalışmalar yapıyorum. Backend mimarisini, veri akışını ve sistemlerin nasıl ölçeklendiğini anlamaya ve uygulamaya odaklanıyorum.</p>
             <p className='aboutme_paragraph'>Hedefim backend tarafında derinleşerek, sağlam ve sürdürülebilir sistemler geliştirebilecek seviyeye ulaşmak.</p>
             </div>
-            <img src='\images\5.png' className='image1'></img>
+            <img src='\images\compImg.png' className='image1'></img>
       </div>
     </section>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 function Footer(){
     return (
         <footer className="footer">
-            <h3>© 2026 Ali Kaan Büke. All rights reserved.</h3>
             <div className="footer-container">
                 <div className="footer-contact">
                     <h2>Contact</h2>
@@ -9,10 +8,11 @@ function Footer(){
                 </div>
                 <div className="social-media">
                     <h2>Follow Me!</h2>
-                    <a href="https://www.instagram.com/bukealikaan/" target="_blank">Instagram</a>
-                    <a href="https://x.com/bukealikaan" target="_blank">X</a>
+                    <a href="https://www.instagram.com/bukealikaan/" target="_blank"><i className="fa-brands fa-instagram" ></i></a>
+                    <a href="https://x.com/bukealikaan" target="_blank"><i className="fa-brands fa-x-twitter"></i></a>
                 </div>
             </div>
+            <h3>© 2026 Ali Kaan Büke. All rights reserved.</h3>
         </footer>
     )
 }
